78035d2e56ef8ba445fd595fe151bb03597944fb
Ilin Tatabitovski <itatabitovski@gmail.com>
2010-07-27 14:15:33 +0200

    Serving of static files during development

--- urls.py
+++ urls.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls.defaults import *
-
+from django.conf import settings
 from django.contrib import admin
 
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-
+    # admin backend
     (r'^admin/', include(admin.site.urls)),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # 
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    )
